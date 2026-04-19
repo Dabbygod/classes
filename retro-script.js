a93402172e303f0bca011e5d5f20b970960618f9
Dabbygod <Dabygod1@Gmail.com>
2026-04-19 09:35:39 -0700

    Update retro-script.js

--- retro-script.js
+++ retro-script.js
@@ -1,8 +1,9 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const gameContainer = document.getElementById('gameContainer');
-    const filteredGames = data.games.filter(game => game.category === "retro");
+const filteredGames = data.games.filter(game =>
+  game.category && game.category.includes("retro")
+);
 
     filteredGames.forEach(game => {
       const gameElement = document.createElement('div');
