4dda97f61b7977a33111997997beadca1d1eccb3
Dabbygod <Dabygod1@Gmail.com>
2026-04-19 09:34:13 -0700

    Update driving-script.js

--- driving-script.js
+++ driving-script.js
@@ -1,8 +1,9 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const gameContainer = document.getElementById('gameContainer');
-    const filteredGames = data.games.filter(game => game.category === "driving");
+    const filteredGames = data.games.filter(game =>
+  game.category && game.category.includes("driving")
+);
 
     filteredGames.forEach(game => {
       const gameElement = document.createElement('div');
