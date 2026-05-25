3c370abc564a8771ccc1c41075b0fac37707d5d4
Dabbygod <Dabygod1@Gmail.com>
2026-05-25 16:50:07 -0700

    Update exitkey.js

--- exitkey.js
+++ exitkey.js
@@ -7,14 +7,11 @@
     window.addEventListener("keydown", (e) => {
         const key = getExitKey();
 
-        // Don't trigger while typing
         const active = document.activeElement;
         if (active && (active.tagName === "INPUT" || active.tagName === "TEXTAREA")) return;
 
-        // If already on index.html → do nothing
         if (window.location.pathname.endsWith("index.html")) return;
 
-        // Otherwise → go home
         window.location.href = "../index.html";
     });
 
